82c96f93fa9d49ff28a98a56a7d2e03b7201d19d
MelianGithub <e_melian04@hotmail.com>
2023-04-13 20:38:08 -0300

    arreglando js

--- js/navBar.js
+++ js/navBar.js
@@ -1,20 +1,34 @@
 const header = document.querySelector('#header');
 let prevScrollPos = window.scrollY;
+let lastKnownScrollPosition = 0;
+let ticking = false;
 
 window.addEventListener('scroll', () => {
-    const currentScrollPos = window.scrollY;
-    if (currentScrollPos > prevScrollPos) {
-        // scroll down
+  lastKnownScrollPosition = window.scrollY;
+  if (!ticking) {
+    window.requestAnimationFrame(() => {
+      if (lastKnownScrollPosition > prevScrollPos && lastKnownScrollPosition > 0 && lastKnownScrollPosition < document.body.scrollHeight - window.innerHeight + 100) {
+        // scroll down and not at the bottom or top
         header.classList.add('hidden');
-    } else {
-        // scroll up
+      } else if (lastKnownScrollPosition < prevScrollPos || lastKnownScrollPosition === 0 || lastKnownScrollPosition >= document.body.scrollHeight - window.innerHeight) {
+        // scroll up or at top or at bottom
         header.classList.remove('hidden');
-    }
-    prevScrollPos = currentScrollPos;
+      }
+      prevScrollPos = lastKnownScrollPosition;
+      ticking = false;
+    });
+    ticking = true;
+  }
 });
 
-
-
-
-
-
+let timeoutId;
+window.addEventListener('scroll', () => {
+  clearTimeout(timeoutId);
+  timeoutId = setTimeout(() => {
+    if (window.scrollY > 0 && window.scrollY < document.body.scrollHeight - window.innerHeight) {
+      header.classList.add('hidden');
+    } else if (window.scrollY >= document.body.scrollHeight - window.innerHeight) {
+      header.classList.add('hidden');
+    }
+  }, 2000);
+});
