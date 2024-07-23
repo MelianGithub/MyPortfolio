fa622b7f9e425804733321163106a763fd505489
MelianGithub <e_melian04@hotmail.com>
2024-07-23 11:25:21 -0300

    Formulario funcionando

---
+++ js/sendemail.js
@@ -0,0 +1,15 @@
+function enviarEmail(event) {
+  event.preventDefault();
+  let parms = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    message: document.getElementById("message").value,
+  };
+
+  emailjs
+    .send("service_xqyogzk", "template_ec6nn0n", parms)
+    .then((response) => {
+      console.log("SUCCESS!", response.status, response.text);
+      window.location.href = "/";
+    });
+}
